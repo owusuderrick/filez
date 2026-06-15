04e37c8752bd07436e022772d621a3ffed1494db
Derrick Owusu <owusuderek@gmail.com>
2026-06-15 01:17:04 -0700

    Completed

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,22 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  import db from "#db/client";
+
+  import { createFile } from "#db/queries/files";
+  import { createFolder } from "#db/queries/folders";
+
+  await db.connect();
+  await seed();
+  await db.end();
+  console.log("🌱 Database seeded.");
+
+  async function seed() {
+    for (let i = 1; i <= 3; i++) {
+      const folder = await createFolder("Folder " + i);
+      for (let j = 1; j <= 5; j++) {
+        await createFile("File " + j, 1000 * j, folder.id);
+      }
+    }
+  }
 }
